9ca20430ec439100410f8a6aa154786fff3a931d
Shubham Liman <shubhamlstudymail@gmail.com>
2025-05-31 12:26:43 +0530

    cors update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://snsacadamy.vercel.app/"],
+    origin: ["http://localhost:5173", "https://snsacadamy.vercel.app"],
     credentials: true, // Allow cookies to be sent from the frontend
   })
 );
